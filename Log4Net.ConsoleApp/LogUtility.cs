31072622b6ce5fe401937e443df1e6ac046262da
Saurav Jha <sauravjha5299@gmail.com>
2024-09-03 13:58:25 +0530

    Log4Net

---
+++ Log4Net.ConsoleApp/LogUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using System.Reflection;
+
+namespace Log4Net.ConsoleApp
+{
+	public class LogUtility
+	{
+        public static void ConfigureLog4Net()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository);
+        }
+    }
+}
+
---
+++ Log4Net.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace Log4Net.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogUtility.ConfigureLog4Net(); // Call the configuration method
+
+            ILogger logger = new Logger(); // Instantiate the Logger class
+
+            logger.Info("Starting the console application");
+
+            try
+            {
+                logger.Debug($"Starting {MethodBase.GetCurrentMethod()?.DeclaringType}");
+                throw new Exception("Sample Error inside the try catch block code");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message, ex.InnerException);
+            }
+
+            Console.ReadLine();
+
+            logger.Info("Ending application");
+        }
+    }
+}
